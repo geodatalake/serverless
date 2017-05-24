4474f348130cbd343df8096dd57a3b433122a06f
Apurv Parikh <apurvparikh1@gmail.com>
2017-05-24 10:32:05 -0400

    added console and restructured project

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 
 Usage
 ```
-cd api/
 npm install
 ```
 
---
+++ console/lib/app-variables.js
@@ -0,0 +1,4 @@
+var YOUR_USER_POOL_ID = 'us-west-2_qXQpCuYxI';
+var YOUR_USER_POOL_CLIENT_ID = '12s8abk54t5he513if05vhp7ra';
+var APIG_ENDPOINT = 'https://ldl2pvooq5.execute-api.us-west-2.amazonaws.com/prod';
+var APP_VERSION = '0.9.0';
